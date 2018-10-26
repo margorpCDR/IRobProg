fba107f62550893d0ce554ad0a29a2dbadca5371
tiger0421 <taiger0421@yahoo.co.jp>
2018-10-26 17:15:47 +0900

    add pGain

--- main/main.ino
+++ main/main.ino
@@ -3,7 +3,7 @@
 #include <parameters.h>
 
 #define MOTOR_TIME 0.2
-#define ODOMETRY_TIME 0.01
+#define SPEEDCONTROL_TIME 0.01
 
 volatile int enc_val_right = 0, enc_val_left = 0;
 volatile uint8_t enc_prev_right = 0, enc_prev_left = 0;
@@ -11,7 +11,8 @@ double glDeg = 0.00;
 double  Xcur=0.00, Ycur=0.00;
 double distance_right = 0.00, distance_left = 0.00;
 
-double kakunin1=0.00, kakunin2=0.00;
+double kakunin1=35.00, kakunin2=30.00;
+int flg = 0;
 
 void space(){
   Serial.print("  ");
@@ -25,7 +26,7 @@ void setup() {
   attachInterrupt(3, updateEncoder2, CHANGE);
   attachInterrupt(2, updateEncoder2, CHANGE);
 
-  FlexiTimer2::set(ODOMETRY_TIME * 1000, odometry);
+  FlexiTimer2::set(SPEEDCONTROL_TIME * 1000, speedControl);
   FlexiTimer2::start();
   
   Serial.begin(9600);
@@ -61,6 +62,7 @@ void updateEncoder1(){
   }
 
   enc_prev_right = ab;
+  flg = 1;
 }
 
 void updateEncoder2(){
@@ -91,13 +93,14 @@ void updateEncoder2(){
   }
 
   enc_prev_left = cd;
+  flg = 1;
 }
 
-void odometry(){
-  double Xref = 0.00, Yref = 0.00;
+void speedControl(){
+  double Xref = 0.00, Yref = 10000000.00;
   double glOmega = 0.00, glVelocity = 0.00, glRho = 0.00, glTheta = 0.00;
   double rad = 0.00, deg = 0.00, deff_deg = 0.00, velocity = 0.00;
-  double output, pGain = 0.00, iGain = 0.00;
+  double output, pGain = 1.1, iGain = 0.00001;
 
   glOmega = enc_val_right - enc_val_left;     //for omega 誤差(?)出るからパルス=>radは後で
   glDeg += glOmega;
@@ -117,18 +120,23 @@ opposite               => CCW
   enc_val_right = 0;
   enc_val_left = 0;
 
-  Serial.println(Ycur);
+  deff_deg = deg + acos((Yref-Ycur)/sqrt((Xref-Xcur)*(Xref-Xcur)+(Yref-Ycur)*(Yref-Ycur)))*180/PI;
+  if(flg == 1){
+    kakunin1 += deff_deg * pGain + (SPEED_RIGHT_REF-velocity) * iGain;
+    kakunin2 += -deff_deg * pGain + (SPEED_RIGHT_REF-velocity) * iGain;
+//    kakunin1 += deff_deg * pGain;
+//    kakunin2 += -deff_deg * pGain;
+  }
+  flg = 0;
 
-  deff_deg = glDeg + acos((Yref-Ycur)/sqrt((Xref-Xcur)*(Xref-Xcur)+(Yref-Ycur)*(Yref-Ycur)));
-  output = deff_deg * pGain + (SPEED_RIGHT_REF-velocity) * iGain;
-
+  Serial.println(deg);
 }
 
 void goStraight(int PWM_Right, int PWM_Left){
   int i;
-  double motor_Right = PWM_MAX/PWM_Right, motor_Left = PWM_MAX/PWM_Right;
+  int motor_Right = PWM_MAX/PWM_Right, motor_Left = PWM_MAX/PWM_Left;
   for(i = 1; i <= PWM_MAX; i++){
-    if(int(i%motor_Right) == 0){
+    if((i%motor_Right) == 0){
       PORTG |= B00100000;
 //      digitalWrite(4,HIGH);
     }
@@ -136,7 +144,8 @@ void goStraight(int PWM_Right, int PWM_Left){
       PORTG &= ~B00100000;
 //      digitalWrite(4,LOW);
     }
-    if(int(i%motor_Left) == 0){
+
+    if((i%motor_Left) == 0){
       PORTH |= B00001000;
 //      digitalWrite(6, HIGH);
     }
@@ -149,35 +158,13 @@ void goStraight(int PWM_Right, int PWM_Left){
 
 
 void loop(){
-
-if(glDeg<0){
-  kakunin1--;
-  kakunin2++;
-}
-else if(glDeg>0){
-  kakunin1++;
-  kakunin2--;
-}
-
-  if(Ycur>-500.00){
-    goStraight(40, 40);
-  }
-  else{
-    goStraight(0, 0);
-  }
-
 /*
-  if(abs(Ycur)<500.00){
-    analogWrite(4,30+kakunin2);
-    analogWrite(5,0);
-    analogWrite(6,30+kakunin1);
-    analogWrite(7,0);
+  if(Ycur>-500.00){
+    goStraight(kakunin1, kakunin2);
   }
   else{
-    analogWrite(4,0);
-    analogWrite(5,0);
-    analogWrite(6,0);
-    analogWrite(7,0);
+    goStraight(1, 1);
   }
 */
+goStraight(kakunin1, kakunin2);
 }
