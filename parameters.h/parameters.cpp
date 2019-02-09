18f1fb7388e4caeb5ad1627a5e3986b262f9f149
MasayaOkada <40891598+MasayaOkada@users.noreply.github.com>
2019-02-09 12:34:26 +0900

    Update parameters.cpp

--- parameters.h/parameters.cpp
+++ parameters.h/parameters.cpp
@@ -5,21 +5,22 @@ parameters::parameters(){
 //encoder setting
 //  DDRD &= ~B00000000;
 
-  pinMode(2, INPUT);        //encoder r
+  pinMode(2, INPUT);        //encoder l
   pinMode(3, INPUT);
-  pinMode(18 , INPUT);        //encoder l
+  pinMode(18 , INPUT);      //encoder r
   pinMode(19 , INPUT);
 
 //motor setting
 
   /*DDRG |= _BV(PG5);
   DDRE |= _BV(PE3);
-  DDRH |= B00011000;/*
-  /*
+  DDRH |= B00011000;*/
+  
+  pinMode(5, OUTPUT);      //Left motor
   pinMode(6, OUTPUT);
-  pinMode(7, OUTPUT);
+  pinMode(7, OUTPUT);      //Right motor
   pinMode(8, OUTPUT);
-  pinMode(11, OUTPUT);
-*/
+  
+
 
 }
