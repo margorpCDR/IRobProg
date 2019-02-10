e48f76fbed6ea7c6dd93939f05003eba756dea8c
Taishi Yamamoto <ambitious0816@gmail.com>
2019-02-10 22:23:18 +0900

    Add files via upload

---
+++ Arduino-S11059-master/README.md
@@ -0,0 +1,14 @@
+# Arduino Library For S11059
+
+[![Build Status](https://travis-ci.org/jakalada/Arduino-S11059.svg?branch=master)](https://travis-ci.org/jakalada/Arduino-S11059)
+
+S11059 (A color sensor) library for Arduino.
+
+## Install
+
+[Download the latest library release.](https://github.com/jakalada/Arduino-S11059/releases/latest) Or [Use the Library Manager.](https://www.arduino.cc/en/Guide/Libraries#toc3)
+
+## Examples
+
+[Please see example code.](./examples)
+
---
+++ Arduino-S11059-master/examples/StandbyFunctionTest/StandbyFunctionTest.ino
@@ -0,0 +1,65 @@
+/**********************************************************************
+  Sensor Datasheet:
+    - EN: http://www.hamamatsu.com/resources/pdf/ssd/s11059-02dt_etc_kpic1082e.pdf
+          (Cat. No. KPIC1082E05 Nov. 2015 DN)
+    - JP: http://www.hamamatsu.com/resources/pdf/ssd/s11059-02dt_etc_kpic1082j.pdf
+          (Cat. No. KPIC1082J05 Nov. 2015 DN)
+**********************************************************************/
+
+#include <Wire.h>
+#include <S11059.h>
+
+S11059 colorSensor;
+
+void setup() {
+  Serial.begin(9600);
+  Wire.begin();
+
+  colorSensor.setMode(S11059_MODE_FIXED);
+  colorSensor.setGain(S11059_GAIN_HIGH);
+  colorSensor.setTint(S11059_TINT3);
+  if (!colorSensor.reset()) {
+    Serial.println("reset failed");
+  }
+
+  delay(500);
+}
+
+void printValues() {
+  for (int i = 0; i < 10; i++) {
+    delay(500);
+    if (colorSensor.update()) {
+      Serial.print(colorSensor.getRed());
+      Serial.print(",");
+      Serial.print(colorSensor.getGreen());
+      Serial.print(",");
+      Serial.print(colorSensor.getRed());
+      Serial.print(",");
+      Serial.print(colorSensor.getIR());
+      Serial.println("");
+    } else {
+      Serial.println("update(): failed");
+    }
+  }
+}
+
+void loop() {
+  // 動作が開始され更新されることを確認する
+  Serial.println("----- not standby");
+  if (!colorSensor.start()) {
+    Serial.println("start(): failed");
+  }
+  Serial.print("isStandby(): ");
+  Serial.println(colorSensor.isStandby());
+  printValues();
+
+  // 待機モードに入り値が更新されないことを確認する
+  Serial.println("----- standby");
+  if (!colorSensor.standby()) {
+    Serial.println("standby(): failed");
+  }
+  Serial.print("isStandby(): ");
+  Serial.println(colorSensor.isStandby());
+  printValues();
+}
+
